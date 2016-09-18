5e60969e4b1ead66392c5994240b4026ce84f2bf
ChienHsin-Chan <hcchien@gmail.com>
2016-09-18 12:06:39 +0800

    add the sections-latest endpoint

--- server.py
+++ server.py
@@ -60,6 +60,27 @@ app.on_insert_accounts += add_token
 app.on_fetched_resource_posts += before_returning_posts
 app.on_fetched_resource_choices += before_returning_choices
 
+@app.route("/sections-latest", methods=['GET'])
+def get_sections_latest():
+    response = { "_items": {}, 
+                 "_links": { 
+                            "self": { "href":"sections-latest", "title": "sections latest"}, 
+                            "parent":{ "parend": "/", "title": "Home" } } }
+    headers = dict(request.headers)
+    tc = app.test_client()
+    resp = tc.get('/sections', headers=headers)
+    resp_header = dict(resp.headers)
+    del headers['Content-Length']
+    resp_data = json.loads(resp.data)
+    if ("_error" not in resp_data and "_items" in resp_data):
+        for item in resp_data["_items"]:
+            sec_resp = tc.get('/posts?where={"sections":"' + item['_id'] + '"}&max_results=5', headers=headers)
+            sec_items = json.loads(sec_resp.data)
+            if '_error' not in sec_items and "_items" in sec_items:
+                #response[item['name']] = sec_items['_items']
+                response['_items'][item['name']] = sec_items['_items']
+    return Response(json.dumps(response), headers=resp_header)        
+        
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080, threaded=True, debug=True)
