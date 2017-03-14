f73ee9c57bf32c13c9fae7acd1f8f2a93c213c0a
ChienHsin-Chan <hcchien@gmail.com>
2017-03-15 01:17:50 +0800

    update the video schema

--- settings.sample.py
+++ settings.sample.py
@@ -593,7 +593,7 @@ videos_schema = {
     'schema': {
         'type': 'objectid',
         'data_relation': {
-            'resource': 'meta',
+            'resource': 'videos',
             'field': '_id',
             'embeddable': True
          },
