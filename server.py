51fbfd5ef34e77d82375b4088c4d44053bab0ccd
Hsin-chan Chien <hcchien@MM-CP01050002.local>
2019-04-30 16:13:38 +0800

    remove the redis support

--- server.py
+++ server.py
@@ -339,8 +339,6 @@ def get_timeline(topicId):
 
 @app.route("/combo", methods=['GET'])
 def handle_combo():
-    global redis_read 
-    global redis_write
     endpoints = {'posts': '/posts?sort=-publishedDate&clean=content&where={"style":{"$nin":["projects", "readr"]}}', 'sectionfeatured': '/sections-featured?content=meta', 'choices': '/choices?max_results=1&sort=-pickDate',\
      'meta': '/meta?sort=-publishedDate&clean=content&related=full', 'sections': '/sections?sort=sortOrder&max_results=20', 'topics':'/topics?sort=sortOrder&max_results=12', 'posts-vue': '/listing?sort=-publishedDate&clean=content&max_results=20&related=false', 'projects': 'listing?where={"style":{"$in":["projects", "readr"]}}&sort=-publishedDate'}
     response = { "_endpoints": {}, 
@@ -348,9 +346,9 @@ def handle_combo():
                             "self": { "href":"sections-latest", "title": "sections latest"}, 
                             "parent":{ "parent": "/", "title": "Home" } } }
     headers = dict(request.headers)
-    cached = redis_read.get(request.url)
-    if cached is not None:
-        return Response(json.dumps(cached), headers=headers)
+    #cached = redis_read.get(request.url)
+    #if cached is not None:
+    #    return Response(json.dumps(cached), headers=headers)
     tc = app.test_client()
     req = request.args.getlist('endpoint')
     for action in req:
@@ -372,7 +370,7 @@ def handle_combo():
     # If there is no request args for endpoint, set the header Content-Type to json
     if not ('Content-Type' in headers and headers['Content-Type'] == "application/json"):
        headers['Content-Type'] = "application/json" 
-    redis_write.setex(request.url, 300, response)
+    #redis_write.setex(request.url, 300, response)
     return Response(json.dumps(response), headers=headers)        
 
 @app.route("/posts-alias", methods=['GET', 'POST'])
@@ -413,12 +411,10 @@ def get_posts_byname():
     return r
 
 if __name__ == '__main__':
-    global redis_read 
-    global redis_write
-    redis_read_port = int(REDIS_READ_PORT)
-    redis_write_port = int(REDIS_WRITE_PORT)
-    redis_readPool = redis.ConnectionPool(host = REDIS_READ_HOST, port = redis_read_port, password = REDIS_AUTH)
-    redis_read = redis.Redis(connection_pool=redis_readPool)
-    redis_writePool = redis.ConnectionPool(host = REDIS_WRITE_HOST, port = redis_write_port, password = REDIS_AUTH)
-    redis_write = redis.Redis(connection_pool=redis_writePool)
+    #redis_read_port = int(REDIS_READ_PORT)
+    #redis_write_port = int(REDIS_WRITE_PORT)
+    #redis_readPool = redis.ConnectionPool(host = REDIS_READ_HOST, port = redis_read_port, password = REDIS_AUTH)
+    #redis_read = redis.Redis(connection_pool=redis_readPool)
+    #redis_writePool = redis.ConnectionPool(host = REDIS_WRITE_HOST, port = redis_write_port, password = REDIS_AUTH)
+    #redis_write = redis.Redis(connection_pool=redis_writePool)
     app.run(host='0.0.0.0', port=8080, threaded=True, debug=True)
