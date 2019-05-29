13c6850e563cd35bf1c77e07b83698afef4a2d1b
Hsin-chan Chien <hcchien@MM-CP01050002.local>
2019-05-29 17:01:36 +0800

    try to fix the topic in None Type issues

--- server.py
+++ server.py
@@ -117,7 +117,7 @@ def clean_item(item):
                     del i['css']
                 if 'categories' in i:
                     del i['categories']
-    if 'topics' in item and item['topics'] is not None:
+    if 'topics' in item and isinstance(item['topics'], list):
         for i in item['topics']:
             if isinstance(i, dict):
                 if 'brief' in i and isinstance(i['brief'], dict) and 'draft' in i['brief']:
