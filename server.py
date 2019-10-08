1aeba58e590720b5196d1ba0142409e509a50b26
Hsin-chan Chien <hcchien@gmail.com>
2019-10-08 15:58:16 +0800

    replace the keywords and add the hyperlinks

--- server.py
+++ server.py
@@ -179,13 +179,13 @@ def before_returning_posts(response):
             if 'brief' in item and isinstance(item['brief'], dict) and 'draft' in item['brief']:
                 del item['brief']['draft']
                 if 'brief' in item and isinstance(item['brief'], dict) and 'html' in item['brief']:
-                    item['brief']['html'].replace("鏡週刊", '<a href="https://www.mirrormedia.mg">鏡週刊</a>')
-                    item['brief']['html'].replace("本刊", '<a href="https://www.mirrormedia.mg">本刊</a>')
+                    item['brief']['html'] = item['brief']['html'].replace("鏡週刊", '<a href="https://www.mirrormedia.mg">鏡週刊</a>')
+                    item['brief']['html'] = item['brief']['html'].replace("本刊", '<a href="https://www.mirrormedia.mg">本刊</a>')
             if 'content' in item and isinstance(item['content'], dict) and 'draft' in item['content']:
                 del item['content']['draft']
                 if 'content' in item and isinstance(item['content'], dict) and 'html' in item['content']:
-                    item['content']['html'].replace("鏡週刊", '<a href="https://www.mirrormedia.mg">鏡週刊</a>')
-                    item['content']['html'].replace("本刊", '<a href="https://www.mirrormedia.mg">本刊</a>')
+                    item['content']['html']= item['content']['html'].replace("鏡週刊", '<a href="https://www.mirrormedia.mg">鏡週刊</a>')
+                    item['content']['html'] = item['content']['html'].replace("本刊", '<a href="https://www.mirrormedia.mg">本刊</a>')
             if clean == 'content':
                 if 'brief' in item and isinstance(item['brief'], dict) and 'html' in item['brief']:
                     del item['brief']['html']
