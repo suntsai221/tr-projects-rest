e728bdceb1fe5a7246d95cd14aaa2504508dd836
I-Chiao Hsu <ichiao.hsu@gmail.com>
2020-01-31 16:18:44 +0800

    refactor(server): removed useless comments

--- gevent_web.py
+++
@@ -1,6 +0,0 @@
-# ref: http://flask.pocoo.org/docs/0.10/deploying/wsgi-standalone/
-from gevent.wsgi import WSGIServer
-from server import app
-
-http_server = WSGIServer(('', 80), app)
-http_server.serve_forever()
